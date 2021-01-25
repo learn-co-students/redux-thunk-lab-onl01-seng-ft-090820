5e694831141076aea5884003af50ec924d25b7ee
= <=>
2021-01-24 20:46:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux';
 import App from './App'
+import catsReducer from './reducers/catsReducer';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import catsReducer from './reducers/catsReducer'
 
+let store = createStore(catsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />,
+    </Provider>,
     document.getElementById('root')
 );
