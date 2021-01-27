437bc136306aa461a80f256f4e39dc65a7b2ccd4
LeahESc <leahemily1@gmail.com>
2021-01-27 14:17:00 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { fetchCats } from './actions/catActions'
 class App extends Component {   
   
   componentDidMount() {
-    console.log(this.props)
     this.props.fetchCats()
   }
 
   handleLoading = () => {
-    if (this.props.loading === true){
-      return <div> Purrrlease wait while your images are loading</div>
+    console.log(this.props.loading)
+    if (this.props.loading){
+      return <div>Images are loading...</div>
+    } else {
+      return  < CatList catPics={this.props.catPics} />
     }
   }
 
@@ -20,8 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>CatBook</h1>
-        {this.handleLoading}
-        < CatList catPics={this.props.catPics} /> 
+        {this.handleLoading()}
       </div>
     );
   }
