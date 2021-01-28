6fb2ced05ef4507bf333af45e2983a864456826a
John Zonneveld <jjczon@gmail.com>
2021-01-27 20:01:44 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { fetchCats } from './actions/catActions';
 import CatList from './CatList'
 
 class App extends Component {   
-
+  // when App mounts succesfully call fetchCats() and store the return in this.props
   componentDidMount() {
     this.props.fetchCats()
   }
 
+  // because a fetch is asynchronous we never know when it will complete
+  // because of that we use the props.loading, before we start the actual fetch we can display
+  // a message that content is getting loaded in background
+  // when the fetch is completed the else path will be followed and CatList be called with passing
+  // of the {this.props.catPics} object
   catHandling = () => {
     console.log(this.props.loading)
     if (this.props.loading) {
@@ -23,6 +28,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>CatBook</h1>
+        {/* here we start 'questioning if the fetch is completed ot not */}
         {this.catHandling()}
       </div>
     );
@@ -36,10 +42,11 @@ const mapStateToProps = state => {
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchCats: () => dispatch(fetchCats())
-  }
-}
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     fetchCats: () => dispatch(fetchCats())
+//   }
+// }
  
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+// export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, { fetchCats })(App)
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import catsReducer from './reducers/catsReducer.js';
 
+// createStore needs some more config when adding middleware
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     catsReducer, /* preloadedState, */ composeEnhancers(
