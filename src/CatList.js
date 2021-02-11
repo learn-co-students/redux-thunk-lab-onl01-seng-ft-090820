6959de29db26c0c95a56f8a5fa1a8aea26c5841e
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-02-11 16:58:02 -0700

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,13 @@
 // write your CatList component here
+import React from 'react'
+
+const CatList = ({catPic}) => {
+    return (
+        <div>
+            <p>Cat Pic </p>
+            <div>{catPic.url}</div> 
+        </div>
+    )
+    
+}
+export default CatList;
